5b15688838cd6fa217c405933f6338afd4fac9b3
KenDaveRob@gmail.com <KenDaveRob@gmail.com@ae101cbf-9cf4-c1de-c606-5d6ee2dece28>
2015-04-29 22:36:24 +0000

    Adding revised code to SVN for first time.

---
+++ Game/Space.java
@@ -0,0 +1,37 @@
+package Game;
+
+/*
+ * The Space Class keeps track of a space for the game board.  Each space has a
+ * location set on the game board.
+ */
+
+/**
+ *
+ * @author Derek Ma
+ */
+public class Space {
+    protected int spaceID;
+    
+    /* Constructor */
+    public Space (int location) {
+        this.spaceID = location;
+    }    
+    public Space () {
+        this.spaceID = 0;
+    }
+    
+    /* Initialize Method */
+    public void initialize(int location) {
+        this.setSpaceID(location);
+    }
+    
+    /* Getters */
+    public int getSpaceID () {
+        return spaceID;
+    }
+    
+    /* Setters */
+    public void setSpaceID (int location) {
+        this.spaceID = location;
+    }
+}
