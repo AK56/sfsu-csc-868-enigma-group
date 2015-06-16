8090c32de8e152e299885d1cda4e46fa58134c70
Gurpartap S Gill <gillgur@mail.sfsu.edu>
2015-06-16 15:26:49 -0700

    Database Controller for the Bank table in DB

--- src/java/Database/BankDatabaseController.java
+++ src/java/Database/BankDatabaseController.java
@@ -8,7 +8,6 @@ import Game.BankAccount;
 import java.sql.*;
 import java.util.ArrayList;
 
-
 /***
  * This class is a wrapper for the SQL queries to the database for the Bank and 
  * BankAccount objects. It also creates objects of these classes and save the new
