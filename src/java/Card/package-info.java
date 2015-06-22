96d59db88040d6d80eb50174829d04647767dadc
Gurpartap S Gill <gillgur@mail.sfsu.edu>
2015-06-22 15:49:27 -0700

    Holds all Card classes - To perform the desired job assigned to each Card when Player lands on Card

--- src/java/Card/package-info.java
+++ src/java/Card/package-info.java
@@ -1,6 +1,5 @@
 /***
  * The Card classes represent actions that a Player must take when they draw a Card 
  * after landing on the Community Card space or the Chance Card space on the game board.
-* 
  */
 package Card;
